c024824519ff8fc091723db181fe6685b0769a88
wish15 <43774827+wish15@users.noreply.github.com>
2020-04-16 21:47:33 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ client=TestClient(app)
 def test_root_url():
     response=client.get("/")
     assert response.status_code==200
-    assert response.json()=={"mag":"Hello and wellcome plase goto /login to login"}
+    assert response.json()=={"msg":"Hello and wellcome plase goto /login to login"}
 
 def test_login_with_correct_credentials():
     auth=HTTPBasicAuth(username="wishcode",password="vishal")
